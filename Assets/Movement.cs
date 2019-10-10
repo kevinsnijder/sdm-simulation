f1c3d3554da895bd68412dde36264d4d8db0970b
BuildTools <unconfigured@null.spigotmc.org>
2019-10-10 13:29:42 +0200

    Added project files

---
+++ Assets/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to provide mouse movement for the camera
+/// </summary>
+public class Movement : MonoBehaviour
+{
+    // Things to run when the camera initializes
+    void Start()
+    {
+
+    }
+
+    public float movespeed = 13.0f;
+    public float scrollspeed = 20.0f;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Registered a mouse movement");
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * -movespeed,
+                                           Input.GetAxisRaw("Mouse Y") * Time.deltaTime * -movespeed, 0);
+            }
+            else if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * -movespeed,
+                                           Input.GetAxisRaw("Mouse Y") * Time.deltaTime * -movespeed, 0);
+            }
+            else if (Input.GetAxis("Mouse Y") > 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * -movespeed,
+                                           Input.GetAxisRaw("Mouse Y") * Time.deltaTime * -movespeed, 0);
+            }
+            else if (Input.GetAxis("Mouse Y") < 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * -movespeed,
+                                           Input.GetAxisRaw("Mouse Y") * Time.deltaTime * -movespeed, 0);
+            }
+        }
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            float newscrollpos = Input.mouseScrollDelta.y * Time.deltaTime * scrollspeed;
+            if ((transform.position.z + newscrollpos) <= -2 && (transform.position.z + newscrollpos) >= -15)
+            {
+                Debug.Log("Registered a scroll");
+                transform.position += new Vector3(0, 0, newscrollpos);
+            }
+        }
+
+    }
+}
---
+++ Assets/MqttHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class MqttHandler : MonoBehaviour
+{
+    private MqttClient client;
+    public string brokerHostname = "arankieskamp.com";
+    public TextAsset certificate;
+    static string subTopic = "hello/world";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting to " + brokerHostname);
+        Connect();
+        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+        byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE };
+        client.Subscribe(new string[] { subTopic }, qosLevels);
+        Publish("hello/world", "Hallo wereld!");
+    }
+
+    private void Connect()
+    {
+        Debug.Log("About to connect on '" + brokerHostname + "'");
+        client = new MqttClient(brokerHostname);
+        string clientId = Guid.NewGuid().ToString();
+        try
+        {
+            client.Connect(clientId);
+            Debug.Log("Success!");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Connection error: " + e);
+        }
+    }
+
+    void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        string msg = System.Text.Encoding.UTF8.GetString(e.Message);
+        Debug.Log("Received message from " + e.Topic + " : " + msg);
+    }
+
+    private void Publish(string _topic, string msg)
+    {
+        Debug.Log("Publishing message: \"" + msg + "\" to \""+_topic+"\"");
+        client.Publish(
+            _topic, Encoding.UTF8.GetBytes(msg),
+            MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
